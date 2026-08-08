5ff2f93211537e5e9cd426c6d69c392e33377f45
Radu Andrei Tudorica <raduandreitudorica3@gmail.com>
2026-08-09 00:12:18 +0300

    store: Implement in-memory store with WAL persistence
    Signed-off-by: Radu Andrei Tudorica <raduandreitudorica3@gmail.com>

--- cmd/raftkv/main.go
+++ cmd/raftkv/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+// import (
+//	"flag"
+//	"log"
+//	"os"
+//	"path/filepath"
+//)
+//
+// func main() {
+//	homeDir, err := os.UserHomeDir()
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	path       := filepath.Join(homeDir, ".raftkv", "data")
+//	walPath := flag.String("walPath", path, "path to the write-ahead log file")
+//	port := flag.String("port", "50051", "port the server listens to")
+//	flag.Parse()
+//}
