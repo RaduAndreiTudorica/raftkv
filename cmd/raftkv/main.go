4edfb7ec6cef7d50125f385869dd81f8e31d576a
Radu Andrei Tudorica <raduandreitudorica3@gmail.com>
2026-08-09 00:20:06 +0300

    cmd: Uncomment server main function
    Signed-off-by: Radu Andrei Tudorica <raduandreitudorica3@gmail.com>

--- cmd/raftkv/main.go
+++ cmd/raftkv/main.go
@@ -1,20 +1,22 @@
 package main
 
 // import (
+//
 //	"flag"
 //	"log"
 //	"os"
 //	"path/filepath"
-//)
 //
-// func main() {
-//	homeDir, err := os.UserHomeDir()
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	path       := filepath.Join(homeDir, ".raftkv", "data")
-//	walPath := flag.String("walPath", path, "path to the write-ahead log file")
-//	port := flag.String("port", "50051", "port the server listens to")
-//	flag.Parse()
-//}
+// )
+func main() {
+	// homeDir, err := os.UserHomeDir()
+	//
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//
+	// path       := filepath.Join(homeDir, ".raftkv", "data")
+	// walPath := flag.String("walPath", path, "path to the write-ahead log file")
+	// port := flag.String("port", "50051", "port the server listens to")
+	// flag.Parse()
+}
